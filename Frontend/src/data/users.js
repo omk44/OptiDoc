a9aa8b4dbdaf9e326e2341361d48619f37e98877
Om Kapadiya <omkapadiya34@gmail.com>
2025-08-07 00:42:29 +0530

    refactor:remove dummy data
    remove dummy data of doctor and authentication of routes due to intialization of directly database

--- Frontend/src/data/users.js
+++ Frontend/src/data/users.js
@@ -1,5 +1,5 @@
-export const dummyUsers = [
-     { username: 'patient@email.com', password: 'password123', role: 'patient', name: 'John Doe' },
-     { username: 'doctor@email.com', password: 'password123', role: 'doctor', name: 'Dr. Smith' },
-     { username: 'admin@email.com', password: 'admin123', role: 'admin', name: 'Admin User' },
-   ];
+// export const dummyUsers = [
+//      { username: 'patient@email.com', password: 'password123', role: 'patient', name: 'John Doe' },
+//      { username: 'doctor@email.com', password: 'password123', role: 'doctor', name: 'Dr. Smith' },
+//      { username: 'admin@email.com', password: 'admin123', role: 'admin', name: 'Admin User' },
+//    ];
