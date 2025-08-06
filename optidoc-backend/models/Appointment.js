f3af5c26c344df86434d94a5c5d9c50943b6a1f0
Om Kapadiya <omkapadiya34@gmail.com>
2025-08-07 01:41:18 +0530

    fix:logic changed of docname and patientname stored in appointment when it is booked

--- optidoc-backend/models/Appointment.js
+++ optidoc-backend/models/Appointment.js
@@ -1,11 +1,37 @@
 const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
-  patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
-  doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
-  date: String,
-  time: String,
-  status: { type: String, default: "booked" }
+  patientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+    required: true,
+  },
+  patientUsername: {
+    type: String,
+    required: true,
+  },
+  doctorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+    required: true,
+  },
+  doctorUsername: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["booked", "notbooked"],
+    default: "notbooked",
+  },
 });
 
 module.exports = mongoose.model("Appointment", appointmentSchema);
